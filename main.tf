3e4547ad08a3dbea30342c0f2666045b0a3cc760
peterjk1965 <peter.j.k@hotmail.com>
2024-07-22 15:52:47 -0500

    added name tag

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
+#set provider "aws" and your location of aws terraform creds
 provider "aws" {
   region                   = "us-east-2" # Replace with your desired AWS region
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "terraform"
 }
-
 #ubuntu 
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  #2 add tags to the instance 
+  tags = {
+    Name = "terraform-example1"
+  }
 }
+#
 
 
 
