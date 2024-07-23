c7718e875deb8b8a5054d71b904bd3fc9610bf32
peterjk1965 <peter.j.k@hotmail.com>
2024-07-23 11:07:29 -0500

    update sec grp

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hey there!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   #2 add tags to the instance 
@@ -31,8 +31,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
